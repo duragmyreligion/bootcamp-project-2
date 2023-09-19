7228ae86f7c78fa5405697df610dbeee602dfa55
phil <philippedebiran@gmail.com>
2023-09-19 19:02:50 +0800

    register

---
+++ public/js/register.js
@@ -0,0 +1,24 @@
+const signUp = async (event) => {
+    event.preventDefault();
+
+    const userName = document.querySelector('#userName-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (userName && email && password){
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ userName, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
+
+  document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
